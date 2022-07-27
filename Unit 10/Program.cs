f63dd9c52aa063dbb609ff3fb6e83ed5a3974d45
SDozH <soldatov_alex@bk.ru>
2022-07-27 16:43:47 +0300

    Добавьте файлы проекта.

---
+++ Unit 10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace Unit_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+    public interface ILogger
+    {
+        void Event(string messege);
+        void Error(string messege);
+    }
+    public class Logger : ILogger
+    {
+        public void Error(string messege)
+        {
+            Console.WriteLine(messege);
+        }
+        public void Event(string messege)
+        {
+            Console.WriteLine(messege);
+        }
+    }
+    public interface IWorker
+    {
+        void Work();
+    }
+}
+
---
+++ Unit 10/Worker1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Unit_10
+{
+    public class Worker1: IWorker
+    {
+        ILogger Logger {get;}
+        public Worker1(ILogger logger)
+        {
+
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker1 начал работу ");
+            Thread.Sleep(3000);
+            Logger.Event("Worker1 закончил работу ");
+        }
+    }
+    public class Worker2 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker2(ILogger logger)
+        {
+
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker2 начал работу ");
+            Thread.Sleep(3000);
+            Logger.Event("Worker2 закончил работу ");
+        }
+    }
+    public class Worker3 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker3(ILogger logger)
+        {
+
+        }
+
+        public void Work()
+        {
+            Logger.Event("Worker3 начал работу ");
+            Thread.Sleep(3000);
+            Logger.Event("Worker3 закончил работу ");
+        }
+    }
+
+}
